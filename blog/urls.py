ce17a92d576d943bcdda98b706a9c015a858868c
Diserton <akanymetov@gmail.com>
2019-05-21 23:38:36 +0600

    created posts model, not done migration

--- blog/urls.py
+++ blog/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from .views import index, blog, post
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('blog/', blog),
+    path('post/', post),
+
 ]
---
+++ posts/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+
+class Author(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	profile_img = models.ImageField()
+
+
+	def __str__(self):
+		return self.user.username
+
+
+
+class Category(models.Model):
+	title = models.CharField(max_length=50)
+
+
+	def __str__(self):
+		return self.title
+
+
+
+class Post(models.Model):
+	title = models.CharField(max_length=150)
+	body = models.TextField()
+	pub_time = models.DateTimeField(auto_now_add=True)
+	comment_count = models.IntegerField(default=0)
+	author = models.ForeignKey(Author, on_delete=models.CASCADE)
+	img = models.ImageField()
+	category = models.ManyToManyField(Category)
+
+
+	def __str__(self):
+		return self.title
