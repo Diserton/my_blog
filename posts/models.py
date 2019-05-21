f51c0d4910097a2676abf625d54bebe58dc54782
Diserton <akanymetov@gmail.com>
2019-05-21 23:43:29 +0600

    migration is done

--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth import get_user_model
 
 
 
+User = get_user_model()
+
+
+
 class Author(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	profile_img = models.ImageField()
