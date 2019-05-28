c1d4dd570f0365529b0a0f048a4f1c716bad137d
Diserton <akanymetov@gmail.com>
2019-05-28 23:45:02 +0600

    last normal heroku config 11

--- blog/settings.py
+++ blog/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
