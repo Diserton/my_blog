121992bada27be62fd0df5e0b8645225fa59184b
Diserton <akanymetov@gmail.com>
2019-05-28 19:36:29 +0600

    last normal heroku config 3

--- blog/urls.py
+++ blog/urls.py
@@ -22,6 +22,8 @@ from django.conf.urls.static import static
 from posts.views import *
 
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 
 urlpatterns = [
@@ -42,6 +44,9 @@ urlpatterns = [
 
 ]
 
-if settings.DEBUG == False:
+if settings.DEBUG is False:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
